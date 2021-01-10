f2b4552cac6b0110e84c38204cf079d1adc28f6d
Amardeep Singh <45745505+amardeep-1@users.noreply.github.com>
2021-01-10 18:14:24 -0500

    Update makeTable.py

--- makeTable.py
+++ makeTable.py
@@ -8,6 +8,8 @@ from sqlalchemy.sql import select
 from sqlalchemy import Table, create_engine
 from flask_sqlalchemy import SQLAlchemy
 
+import os
+
 conn = sqlite3.connect('data.sqlite')
 engine = create_engine('sqlite:///data.sqlite')
 db = SQLAlchemy()
@@ -21,4 +23,10 @@ Users_tbl = Table('users', Users.metadata)
 
 def create_users_table():
     Users.metadata.create_all(engine)
+
 create_users_table()
+
+
+os.mkdir('assets')
+os.mkdir('assets\\public_images\\')
+os.mkdir('assets\\user_images\\')
